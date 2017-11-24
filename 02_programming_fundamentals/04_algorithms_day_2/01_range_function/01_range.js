9b8d2be6d187f55f4a42edaef0d170c43df2407f
Fabien Lebas <fabien.lebas@decathlon.com>
2017-11-24 14:30:18 +0100

    Exo 1 OK

--- 02_programming_fundamentals/04_algorithms_day_2/01_range_function/01_range.js
+++ 02_programming_fundamentals/04_algorithms_day_2/01_range_function/01_range.js
@@ -4,8 +4,18 @@
 // This function should work in both ascending or descending order.
 
 // Complete this function.
-function range() {
-
+function range(start, end) {
+  let result = [];
+  if (start <= end){
+    for(let i=0; i <= end-start; i++){
+      result.push(start+i);
+    }
+  } else {
+    for (let i=0; i<= start-end; i++){
+      result.push(start-i);
+    }
+  }
+  return result;
 }
 
 // Do not remove last lines, it is for tests
