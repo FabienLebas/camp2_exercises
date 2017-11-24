fb0b154a5593ff8ef2525796103a93fc3920c824
Fabien Lebas <fabien.lebas@decathlon.com>
2017-11-24 14:48:49 +0100

    Exo2 OK

--- 02_programming_fundamentals/04_algorithms_day_2/02_car_rental/01_index.js
+++ 02_programming_fundamentals/04_algorithms_day_2/02_car_rental/01_index.js
@@ -1,6 +1,6 @@
 const driver = {
-  driverLicense: "B1",
-  licenseIssued: new Date().getFullYear() - 10, // 10 years old license
+  driverLicense: "B",
+  licenseIssued: new Date().getFullYear() - 2, // 10 years old license
   numberOfAccident: 0,
   bonus: 0.8,
 };
@@ -9,7 +9,17 @@ const driver = {
 // * Input: a driver
 // * Output: a boolean if the driver can rent a car
 
+function canRentACar(driver){
+  if ((driver.driverLicense === "B" || driver.driverLicense === "B1")
+  && (2017 - driver.licenseIssued >= 2)
+  && (driver.numberOfAccident === 0 || driver.bonus >= 0.7)){
+    return true;
+  } else {
+    return false;
+  }
+}
 
+console.log(canRentACar(driver));
 
 
 // ⚠ Do not remove me ! It's for tests
