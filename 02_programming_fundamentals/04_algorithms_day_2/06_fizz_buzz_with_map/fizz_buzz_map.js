0949a0a6310ed4101f477861080bd8c397b957e6
Fabien Lebas <fabien.lebas@decathlon.com>
2017-11-24 16:36:12 +0100

    Exo 6 OK

--- 02_programming_fundamentals/04_algorithms_day_2/06_fizz_buzz_with_map/fizz_buzz_map.js
+++ 02_programming_fundamentals/04_algorithms_day_2/06_fizz_buzz_with_map/fizz_buzz_map.js
@@ -9,9 +9,22 @@
    YOU MUST USE array.map
 */
 
-function fizzBuzz(list) {
-
+function fizzTest(number){
+  if (number % 15 === 0){
+    return "FizzBuzz";
+  } else if (number % 3 === 0){
+    return "Fizz";
+  } else if (number % 5 === 0){
+    return "Buzz";
+  } else {
+    return number;
+  }
 }
 
+function fizzBuzz(list) {
+  return list.map(fizzTest);
+}
 
+const test = [1, 2, 3, 4, 5, 6];
+console.log(fizzBuzz(test));
 module.exports = fizzBuzz;
