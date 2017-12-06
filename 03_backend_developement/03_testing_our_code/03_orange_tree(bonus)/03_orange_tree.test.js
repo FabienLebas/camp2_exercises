f5c64554c966edc4531b72b37cbf5839fc30e1fc
Fabien Lebas <fabien.lebas@decathlon.com>
2017-12-06 15:08:23 +0100

    Test orange tree OK

---
+++ 03_backend_developement/03_testing_our_code/03_orange_tree(bonus)/03_orange_tree.test.js
@@ -0,0 +1,117 @@
+const orangeTree = require("./03_orange_tree");
+
+test("Check tree starting state", () => {
+  expect(orangeTree.alive).toBe(true);
+  expect(orangeTree.height).toBe(0);
+  expect(orangeTree.age).toBe(0);
+  expect(orangeTree.oranges).toBe(0);
+});
+
+test("I should not be able to pick oranges before growing",() => {
+  expect(orangeTree.pickAnOrange()).toBe(false);
+});
+
+test("I should be able to pick an orange when there are some", () => {
+  orangeTree.oranges = 1;
+  expect(orangeTree.pickAnOrange()).toBe(true);
+});
+
+test("I should not be able to pick 2 oranges when there is 1", () => {
+  orangeTree.oranges = 1;
+  expect(orangeTree.pickAnOrange()).toBe(true);
+  expect(orangeTree.pickAnOrange()).toBe(false);
+});
+
+test("Seed should reset values", () => {
+  orangeTree.alive = false;
+  orangeTree.height = 1;
+  orangeTree.age = 12;
+  orangeTree.oranges = 10;
+  orangeTree.seed();
+  expect(orangeTree.alive === true &&
+      orangeTree.height === 0 &&
+      orangeTree.age === 0 &&
+      orangeTree.oranges === 0).toBe(true);
+});
+
+test("ageOneYear just after birth", () => {
+  orangeTree.alive = true;
+  orangeTree.ageOneYear();
+  expect(orangeTree.age).toBe(1);
+  expect(orangeTree.height).toBe(25);
+  expect(orangeTree.oranges).toBe(0);
+  expect(orangeTree.alive).toBe(true);
+});
+
+test("Test 4 years old", () =>{
+  orangeTree.seed();
+  for(let i = 1; i <= 4; i++){
+    orangeTree.ageOneYear();
+  }
+  expect(orangeTree.height).toBe(25 * 4);
+  expect(orangeTree.oranges).toBe(0);
+  expect(orangeTree.alive).toBe(true);
+});
+
+test("Test 5 years old", () =>{
+  orangeTree.seed();
+  for(let i = 1; i <= 5; i++){
+    orangeTree.ageOneYear();
+  }
+  expect(orangeTree.height).toBe(25 * 5);
+  expect(orangeTree.oranges).toBe(10);
+  expect(orangeTree.alive).toBe(true);
+});
+
+test("Test 8 years old", () =>{
+  orangeTree.seed();
+  for(let i = 1; i <= 8; i++){
+    orangeTree.ageOneYear();
+  }
+  expect(orangeTree.height).toBe(25 * 8);
+  expect(orangeTree.oranges).toBe(10);
+  expect(orangeTree.alive).toBe(true);
+});
+
+test("Test 10 years old", () =>{
+  orangeTree.seed();
+  for(let i = 1; i <= 10; i++){
+    orangeTree.ageOneYear();
+  }
+  expect(orangeTree.height).toBe(25 * 9 + 10);
+  expect(orangeTree.oranges).toBe(20);
+  expect(orangeTree.alive).toBe(true);
+});
+
+test("Test 20 years old", () =>{
+  orangeTree.seed();
+  for(let i = 1; i <= 20; i++){
+    orangeTree.ageOneYear();
+  }
+  expect(orangeTree.height).toBe(25 * 9 + 10 * 10);
+  expect(orangeTree.oranges).toBe(5);
+  expect(orangeTree.alive).toBe(true);
+});
+
+test("Test 40 years old", () =>{
+  orangeTree.seed();
+  for(let i = 1; i <= 40; i++){
+    orangeTree.ageOneYear();
+  }
+  expect(orangeTree.height).toBe(25 * 9 + 10 * 10);
+  expect(orangeTree.oranges).toBe(0);
+  expect(orangeTree.alive).toBe(true);
+});
+
+test("You should not die before 50, but you should before 100", () =>{
+  orangeTree.seed();
+  for(let i = 1; i <= 50; i++){
+    orangeTree.ageOneYear();
+  }
+  expect(orangeTree.alive).toBe(true);
+
+  for(let i = 50; i <= 100; i++){
+    orangeTree.ageOneYear();
+  }
+  expect(orangeTree.alive).toBe(false);
+});
