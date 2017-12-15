f3afa9f67eee511aca4ae39a12338bdd718e44ed
Fabien Lebas <fabien.lebas@decathlon.com>
2017-12-15 14:38:17 +0100

    Exo1 partie 2 OK

--- 03_backend_developement/10_promises/01_node_fetch/index.js
+++ 03_backend_developement/10_promises/01_node_fetch/index.js
@@ -8,20 +8,51 @@ app.listen(port, function () {
   console.log("Server listening on port:" + port);
 });
 
-function display(object){
+function displayObject(object){
   app.get("/", function (request, result) {
     result.json(object);
   });
 }
 
-function getProductInfos(callback){
-  return fetch("https://decath-product-api.herokuapp.com/products/efe288cb-fb63-4b23-b8df-529f04b8b02b")
-    .then((response) => response.json())
-    .then((result) => callback(result));
+function displayText(text){
+  app.get("/", function (request, result) {
+    result.send(text);
+  });
 }
 
-getProductInfos(display);
+function getProductInfos(product_id){
+  return fetch("https://decath-product-api.herokuapp.com/products/" + product_id)
+    .then((response) => response.json())
+    .then((result) => {
+      displayObject(result);
+      return result;
+    })
+    .catch((error) => {
+      console.warn(error);
+    });
+}
+
+getProductInfos("efe288cb-fb63-4b23-b8df-529f04b8b02b");
+
+function getBrandFromProduct(product_id){
+  return fetch("https://decath-product-api.herokuapp.com/products/" + product_id)
+    .then(response => response.json())
+    .then(object => object.brand_id)
+    .then(brand_id => fetch("https://decath-product-api.herokuapp.com/brands/" + brand_id))
+    .then(response => response.json())
+    .then(object => object.title)
+    .then(title => {
+      displayText(title);
+      return title;
+    })
+    .catch((error) => {
+      console.warn(error);
+    }); 
+}
+
+//getBrandFromProduct("efe288cb-fb63-4b23-b8df-529f04b8b02b");
 
 module.exports = {
-  getProductInfos: getProductInfos
+  getProductInfos: getProductInfos,
+  getBrandFromProduct: getBrandFromProduct
 };
--- 03_backend_developement/10_promises/01_node_fetch/index.test.js
+++ 03_backend_developement/10_promises/01_node_fetch/index.test.js
@@ -3,7 +3,16 @@ const index = require("./index");
 test("getProductInfos should return an object", () => {
   expect.assertions(1);
 
-  return index.getProductInfos((object) => object)
+  return index.getProductInfos("efe288cb-fb63-4b23-b8df-529f04b8b02b")
+    .then(result => {
+      expect(result).toBeTruthy();
+    });
+});
+
+test("getBrandFromProduct should return a text", () => {
+  expect.assertions(1);
+
+  return index.getBrandFromProduct("efe288cb-fb63-4b23-b8df-529f04b8b02b")
     .then(result => {
       expect(result).toBeTruthy();
     });
